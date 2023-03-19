2dac0f6a1ccea21cc65ff5e46187dc3b4ea41834
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-19 13:32:42 +0000

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio>
+#include<stdio.h>
 /**
  * main - A program thats prints a line using printf function
  * Return: 0 (Success)
@@ -8,3 +8,4 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
+
