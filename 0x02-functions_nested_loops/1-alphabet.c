955f72c2b254e35d4177b9ac80d49f43531e711c
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-21 21:41:35 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description - function to print alphabet in lower case
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int k;
+
+	for(k = 97; k <= 122; k++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
