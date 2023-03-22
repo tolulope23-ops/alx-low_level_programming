d6ab5491d8552ae87fe6ab3d4d160ce375b919e6
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-22 21:18:54 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * add - Sum two numbers
+ * @i: the first number
+ * @j: the second number
+ * Return: the addition ofboth
+ */
+int add(int i, int j);
+{
+	return (i + j);
+}
