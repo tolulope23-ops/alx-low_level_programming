10ac4f66af35cf789826b57dc6d864a34b0d8e72
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-22 00:13:56 +0000

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,6 +19,7 @@ void jack_bauer(void)
 				for (d = 0; d <= 9; d++)
 				{
 					if (a >= 2 && b >= 4)
+					{
 						break;
 						_putchar(a + 48);
 						_putchar(b + 48);
@@ -26,6 +27,7 @@ void jack_bauer(void)
 						_putchar(c + 58);
 						_putchar(d + 48);
 						_putchar('\n');
+					}
 				}
 			}
 		}
