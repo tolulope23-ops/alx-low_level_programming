9505e8b61abb8dbfafa924c9cc08021765355f64
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-24 23:39:31 +0000

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_numbers - print numbers from 0 - 9
+ *
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
