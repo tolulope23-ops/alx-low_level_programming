746e7a5c31953e2840b079e439bc3bba5eed725e
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-25 01:16:12 +0000

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,15 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 1; i >= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j >= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i);
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
