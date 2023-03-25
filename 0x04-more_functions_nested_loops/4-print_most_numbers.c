f4dd41b9a4d91e8a781e37bf1dbf7030a32bbe47
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-25 00:00:05 +0000

    4-prinr_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_most_numbers - print numbers 0 - 9 excluding 2 and 4
+ *
+ * Return: 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		if (j == 2 || j == 4)
+		{
+			continue;
+		}
+		_putchar(j);
+	}
+	_putchar('\n');
+}
