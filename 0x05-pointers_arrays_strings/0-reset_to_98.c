e6f293a557d2b903c46cca2af3d169394e73cb98
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-27 20:04:13 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * reset_to_98 - updates the value its points to to 98
+ *
+ * @n: the value to be updated to 98
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
