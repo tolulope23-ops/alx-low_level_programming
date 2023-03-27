2df8f2a633457cef01559cb0fd87dd9ff7209b73
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-27 20:41:54 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * _puts - function that print a string
+ *
+ * @str: pointer to the string
+ *
+ * Return: string and a new line
+ */
+void _puts(char *str)
+{
+	int j = 0;
+
+	while (str[j])
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
