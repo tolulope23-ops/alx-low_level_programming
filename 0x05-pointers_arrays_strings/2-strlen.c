d9de2001508aa7e55a3ad583680f28a7e493ad01
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-27 20:30:30 +0000

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _strlen - checks the length of a particular string
+ *
+ * @s: string to be checked
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int str_length = 0;
+
+	while (s[str_length])
+		str_length++;
+	return (str_length);
+}
