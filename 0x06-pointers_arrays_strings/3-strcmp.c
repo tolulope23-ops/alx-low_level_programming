3d79c8eed03047a0b5610a751bf012b4f4899535
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-29 21:02:16 +0000

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * _strcmp - function that compares two string
+ *
+ * @s1: first string
+ *
+ * @s2: second character
+ *
+ * Return: less than 0 if string is less than the other,
+ * greater than 0 if string is greater than the other
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int count;
+	int cmp_value;
+
+	count = 0;
+	while (s1[count] == s2[count] && s1[count] != '\0')
+	{
+		count++;
+	}
+	cmp_value = s1[count] - s2[count];
+	return (cmp_value);
+}
