7e9f2e34b6b42c6ed742dba9a37457348ab60dbf
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-03-29 20:00:19 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strcat - function that concatenates two strings
+ *
+ * @dest: destination string pointer
+ *
+ * @src: source string pointer
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int length_of_string;
+	int a;
+
+	length_of_string = 0;
+	while (dest[length_of_string] != '\0')
+	{
+		length_of_string++;
+	}
+	for (a = 0; src[a] != '\0'; a++,length_of_string++)
+	{
+		dest[length_of_string] = src[a];
+	}
+	dest[length_of_string] = '\0';
+	return (dest);
+}
