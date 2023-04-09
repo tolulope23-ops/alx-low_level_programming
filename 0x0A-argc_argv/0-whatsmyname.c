e805effc45551ad107068cc86f09c88a08c99293
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-04-09 13:38:40 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - Function that print the name of the program
@@ -11,5 +12,5 @@ int main(int argc, char *argv[])
 	(void) argc;
 	printf("%s\n", argv[0]);
 
-	return (0)
+	return (0);
 }
