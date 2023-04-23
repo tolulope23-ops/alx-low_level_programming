14b589ef34e489de609d447ccb02412caec1bd70
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2023-04-23 14:07:08 -0700

    function that concatenates all the arguments of a program

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include"main.h"
+/**
+ * argstostr - main point
+ * @ac: integer input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, n, r = 0, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			k++;
+	}
+	k += ac;
+
+	str = malloc(sizeof(char) * k + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
+}
